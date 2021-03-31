89e2b9195585ac379c0c075872a9348c0475584c
ironlung13 <ironlung13@mail.ru>
2021-03-31 19:22:22 +0300

    Fix

--- XMLTasks.cs
+++ XMLTasks.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Linq;
-using System.Xml;
 
 namespace LabWork_LINQ_XML
 {
     public static class XMLTasks
     {
-        public static void XMLTask1_6(string textFilePath = @"Text Files\InputFileTask1.txt", string XMLFilePath = @"Text Files\Task1.xml")
+        public static void XMLTask1_6(string textFilePath = @"Text Files\InputFileTask1.txt", string XMLFilePath = @"Text Files\Task1Result.xml")
         {
            /*Даны имена существующего текстового файла и
              создаваемого XML-документа. Каждая строка текстового
@@ -61,7 +60,7 @@ namespace LabWork_LINQ_XML
                 var lineData = new XElement("line");
                 //Добавляем к нему аттрибут Sum
                 lineData.Add(new XAttribute("Sum", sum));
-                //Добавляем в элемент line все полученные числа как элементы 2 -го уровня Number
+                //Добавляем в элемент line все полученные числа как элементы 2-го уровня Number
                 foreach(var number in sortedNums)
                 {
                     lineData.Add(new XElement("Number", number));
@@ -71,6 +70,7 @@ namespace LabWork_LINQ_XML
             }
             //Сохраняем в XML файл
             data.Save(XMLFilePath);
+
             Console.WriteLine($"Сохранено в файл {XMLFilePath}.");
             Console.WriteLine("Вывод:");
             Console.WriteLine(data);
@@ -143,7 +143,7 @@ namespace LabWork_LINQ_XML
             //Удаляем эти аттрибуты
             edited.Remove();
             //Сохраняем преобразованный файл
-            document.Save(@"Text Files\Sample_1_Edited.xml");
+            document.Save(@"Text Files\Task2Result.xml");
         }
 
         public static void XMLTasks4_46(string inputXMLFilePath = @"Text Files\Sample_1.xml")
@@ -166,7 +166,8 @@ namespace LabWork_LINQ_XML
             {
                 element.Add(new XAttribute("odd-node-count", false));
             }
-            document.Save(@"Text Files\Sample_2_edited.xml");
+            //Сохраняем
+            document.Save(@"Text Files\Task3Result.xml");
         }
 
         public static void XMLTasks5_58(string S, string inputXMLFilePath = @"Text Files\Sample_1.xml")
@@ -178,16 +179,48 @@ namespace LabWork_LINQ_XML
               xml:count со значением, равным количеству потомковэлементов для данного элемента (xml — префикс пространства имен XML).
               Указание. Использовать свойство Xml класса XNamespace*/
 
+            //Загружаем документ
             var document = XDocument.Load(inputXMLFilePath);
+            //Определяем пространство имен
             XNamespace ns = S;
+            //Добавляем префикс и описание пространства имен в корневой элемент
             document.Root.Name = ns + document.Root.Name.ToString();
             document.Root.Add(new XAttribute(XNamespace.Xmlns + "node", S));
+            //Для всех элементов первого уровня добавляем аттрибуты
             foreach (var element in document.Root.Elements())
             {
                 element.Add(new XAttribute(ns + "count", element.DescendantNodes().Count()));
                 element.Add(new XAttribute(XNamespace.Xml + "count", element.Descendants().Count()));
             }
-            document.Save(@"Text Files\Sample_3_edited.xml");
+            //Сохраняем
+            document.Save(@"Text Files\Task4Result.xml");
+        }
+
+        public static void XMLTasks6_76(string inputXMLFilePath)
+        {
+            /*Дан XML-документ с информацией о задолженности по оплате коммунальных услуг. Образец элемента первого уровня:
+              <record>
+                <house>12</house>
+                <flat>129</flat>
+                <name>Сергеев Т.М.</name>
+                <debt>1833.32</debt>
+              </record>
+
+              Здесь house — номер дома (целое число), flat — номер
+              квартиры (целое число), name — фамилия и инициалы
+              жильца (инициалы не содержат пробелов и отделяются от
+              фамилии одним пробелом), debt — размер задолженности в
+              виде дробного числа: целая часть — рубли, дробная часть —
+              копейки (незначащие нули не указываются). 
+              Преобразовать документ, изменив элементы первого
+              уровня следующим образом:
+
+              <debt house="12" flat="129">
+                <name>Сергеев Т.М.</name>
+                <value>1833.32</value>
+              </debt>
+
+              Порядок следования элементов первого уровня не изменять*/
         }
     }
 }
